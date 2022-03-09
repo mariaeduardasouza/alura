e9f9843b78f6f8396d2a610ef77fa9f8d29536e2
Maria Eduarda Da Silva Souza <71784514+mariaeduardasouza@users.noreply.github.com>
2022-03-09 09:45:46 -0300

    mentalista- aula3/alura(jogo de acertar o numero aleatorio)

---
+++ js/aula3.js
@@ -0,0 +1,16 @@
+
+
+function Chutar(){
+    const numeroSecreto = parseInt(Math.random() * 11);
+    const resultado = document.getElementById("resultado")
+    const chute = parseInt(document.getElementById("valor").value);
+
+    if(chute == numeroSecreto){
+        resultado.innerHTML = "Acertou, o numero secreto era:" + numeroSecreto;
+    }else if(chute > 10 || chute < 0){
+        resultado.innerHTML = "Digite um numero de 0 a 10";
+    }else{
+        resultado.innerHTML = "Errou, o numero secreto era:" + numeroSecreto;
+    }
+
+}
